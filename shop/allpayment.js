b5e59f8db713519eda44b749cb2d9f7cefdda5ba
chandrawiguna99 <chndrwgnn66@gmail.com>
2022-02-01 03:50:34 +0700

    bot

---
+++ shop/allpayment.js
@@ -0,0 +1,24 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 1142 3262 
+2. OVO
+	ㅁ 0812 1142 3262
+3. DANA
+	ㅁ 0812 1142 3262
+	
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,21 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 1142 3262
+2. OVO
+	ㅁ 0812 1142 3262
+3. DANA
+	ㅁ 0812 1142 3262
+	
+
+
+`
+	}
+
+exports.donasibot = donasibot
+ 
